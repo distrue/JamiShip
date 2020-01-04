57b82e354f05a970179a1eecd10901b19b06437e
bor1bori <bor1boridv@gmail.com>
2020-01-05 02:41:19 +0900

    게임리스트에 raindrop 추가

--- JamiShip/gameList.ts
+++ JamiShip/gameList.ts
@@ -24,8 +24,28 @@ function loop() {
   tutorial: [],
 };
 
+const raindrop = {
+  title: '비내리기',
+  image: 'https://www.miracle-recreation.com/content/uploads/2018/11/Image-Header_Park.jpg',
+  id: 'raindrop',
+  desc: '비를 내려 고양이를 목욕시키세요',
+  stub: `function setup() {
+  setGame("raindrop");
+}
+
+function init() {
+
+}
+
+function loop() {
+
+}`,
+  tutorial: [],
+};
+
 export const GameDisplay: GameEntry[] = [
   playground,
+  raindrop,
 ];
 
 export const EmptyGame = playground;
