43b84cd9b2481e619dcc6d9173b7b9471bf63207
Junghoon Noh <jungnoh.dev@gmail.com>
2020-01-04 20:54:38 +0900

    Add Logger styles and auto scroll

--- components/Logger.tsx
+++ components/Logger.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 export interface LogItem {
@@ -12,23 +12,39 @@ interface LoggerProps {
 }
 
 const Logger = (props: LoggerProps) => {
+  const endRef: React.RefObject<HTMLDivElement> = React.createRef();
   const mapLogItem = (v: LogItem, i: number) => (
     <div className={v.level} key={i}>{v.value}</div>
   );
+  useEffect(() => {
+    endRef.current!.scrollIntoView();
+  })
   return (
     <Container>
       {props.logData.map(mapLogItem)}
+      <div ref={endRef} />
     </Container>
   );
 };
 
 const Container = styled.div`
-  .log {}
+  .log {
+    color: #FFF;
+  }
   .warn {
     color: rgb(225, 192, 76);
   }
   .error {
     color: #FF0000;
+  }
+  & {
+    background-color: #222;
+    width: 100%;
+    height: 100%;
+    padding: 8px;
+    box-sizing: border-box;
+    overflow-x: wrap;
+    overflow-y: scroll;
   }
 `;
 
