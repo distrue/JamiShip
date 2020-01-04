140343b3ef0a380a0af9357b6d8a4f181e7a8f55
Y.Jung <gilsang9808@gmail.com>
2020-01-04 16:11:35 +0900

    modulize component, engine library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import engine from '../runtimeEngine';
+import engine from '../JamiShip/engine';
+import { BaseObj, checkRectOverlap } from '../JamiShip/component';
 
 export default () => {
   React.useEffect(() => {
     engine();
+    const testBase = new BaseObj('map-canvas', ['https://cdn.auth0.com/blog/react-js/react.png'], true, { x: 100, y: 100 });
+    const testBase2 = new BaseObj('map-canvas', ['https://cdn.auth0.com/blog/react-js/react.png'], true, { x: 100, y: 100 }, { x: 80, y: 80 });
+    console.log(testBase.getInnerRect());
+    console.log(checkRectOverlap(testBase.getInnerRect(), testBase2.getInnerRect()));
   }, []);
 
   return (
-    <Background>
-      <div className="canvas">
+    <>
+      <canvas id="map-canvas" width="300px" height="300px">
         canvas
-      </div>
-      <div className="cli" />
-      <div className="state">
-        state
-      </div>
-    </Background>
+      </canvas>
+      <Background>
+        <div className="cli" />
+        <div className="state">
+            state
+        </div>
+      </Background>
+    </>
   );
 };
 
 const Background = styled.div`
   width: 100vw;
   height: 100vh;
-  position: relative;
-  .canvas {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 65vw;
-    height: 70vh;
-    border: 1px solid black;
-  }
+  position: absolute;
+  top: 0; left: 0;
   .cli {
     position: absolute;
     top: 0px;
