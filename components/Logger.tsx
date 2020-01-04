388980091d6395a6354fd5b0592e4196b773bf1c
Junghoon Noh <jungnoh.dev@gmail.com>
2020-01-04 16:36:21 +0900

    Add working logger

---
+++ components/Logger.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface LogItem {
+  level: 'log' | 'warn' | 'error';
+  value: string;
+}
+
+interface LoggerProps {
+  count: number;
+  logData: any;
+}
+
+const Logger = (props: LoggerProps) => {
+  console.log(props.count);
+  const mapLogItem = (v: LogItem, i: number) => (
+    <div className={v.level} key={i}>{v.value}</div>
+  )
+  return (
+    <Container>
+      {props.logData.map(mapLogItem)};
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .log {}
+  .warn {
+    color: rgb(225, 192, 76);
+  }
+  .error {
+    color: #FF0000;
+  }
+`;
+
+export default Logger;
---
+++ pages/code.tsx
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import CodeEditor from '../components/CodeEditor';
+import injectCode from '../lib/inject';
+import { build, ForeignCode } from '../lib/execute';
+import Logger, {LogItem} from '../components/Logger';
+
+const defaultCode = `const a = 123;
+const b = 234;
+let c = 'AAA';
+
+const init = () => {
+  logger.log(a+b);
+};
+function loop() {
+  logger.log(c);
+}`;
+
+const CodePage = (): JSX.Element => {
+  const [codeValue, setCodeValue] = useState(defaultCode);
+  const [codeObj, setCodeObj] = useState<ForeignCode | null>(null);
+  const [log, setLog] = useState(0);
+  const [logData, setLogData] = useState<LogItem[]>([]);
+
+  const logFactory = (level: 'log' | 'warn' | 'error', value: string) => {
+    const data = logData;
+    const cnt = data.push({level, value});
+    setLog(cnt);
+    setLogData(data);
+  }
+
+  const injector = (): string => {
+    return injectCode({
+      logger: {
+        log: (v) => logFactory('log', v),
+        warn: (v) => logFactory('warn', v),
+        error: (v) => logFactory('error', v)
+      }
+    });
+  }
+
+  const loadHandler = () => {
+    const injectCode = injector();
+    setCodeObj(build(codeValue, injectCode));
+  };
+  const initHandler = () => {
+    if (codeObj === null) {
+      alert('Code is not loaded!');
+      return;
+    }
+    codeObj.init();
+  };
+  const loopHandler = () => {
+    if (codeObj === null) {
+      alert('Code is not loaded!');
+      return;
+    }
+    codeObj.loop();
+  };
+
+  return (
+    <Container>
+      <CodeEditor onChange={setCodeValue} value={codeValue}
+        style={{
+          gridRow: "1 / 3",
+          gridColumn: "1 / 2",
+          width: "100%",
+          height: "100%"
+        }}/>
+      <div className="controls">
+        <button onClick={loadHandler}>Reload</button>
+        <button onClick={initHandler}>Init()</button>
+        <button onClick={loopHandler}>Loop()</button>
+      </div>
+      <div className="display">
+        <Logger count={log} logData={logData} />
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  & {
+    display: grid;
+    height: 100vh;
+    grid-template-columns: auto 16px auto;
+    grid-template-rows: 36px auto;
+    box-sizing: border-box;
+  }
+  .code {
+    grid-row: 1 / 3;
+    grid-column: 1 / 2;
+  }
+  .display {
+    grid-row: 2 / 3;
+    grid-column: 3 / 4;
+  }
+  .controls {
+    grid-row: 1 / 2;
+    grid-column: 3 / 4;
+  }
+`;
+
+export default CodePage;
